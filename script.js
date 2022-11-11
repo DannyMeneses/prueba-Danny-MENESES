e5cad1c06bdcc32e700b150a950fd4edf661c003
DannyMeneses <104269119+DannyMeneses@users.noreply.github.com>
2022-11-11 10:29:27 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function updateheader(){
+    function mainFunction(printText) {
+        document.getElementById("caja")
+        caja="CABECERA ACTUALIZADA";
+    }
+    function printText() {
+        document.getElementById("caja")
+        alert = caja();
+        if (!$('caja').val()) {
+            alert('Ups! Nada que mostrar');
+        }
+    }
+}
+return caja,alert;
+
+function mainFunction(printText) {
+    document.getElementById("caja")
+    caja="CABECERA ACTUALIZADA";
+}
+function printText() {
+    document.getElementById("caja")
+    alert = caja();
+    if (!$('caja').val()) {
+        alert('Ups! Nada que mostrar');
+    }
+}
